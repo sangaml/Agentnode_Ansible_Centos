39b4ad8c854251e33b17307051113e5ed572381d
Sangam <36110390+sangaml@users.noreply.github.com>
2019-06-08 23:01:39 +0530

    Update and rename ansible-tower.sh to ansibleTower_azurecli.sh

--- ansibleTower_azurecli.sh
+++ ansibleTower_azurecli.sh
@@ -28,7 +28,23 @@ install_AnsibleTower() {
     echo -e "$1 installed"
 }
 
+install_AzureCli() {
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+    sudo yum install azure-cli -y
+    yum install python-pip python-wheel -y
+    pip install --upgrade pip
+    pip install packaging
+    pip install "azure==2.0.0"
+    pip install packaging
+    pip install msrestazure
+    pip install --upgrade msrestazure
+    pip install --ignore-installed ansible[azure]
+}
+
 
 yum update -y >>$LOGFILE 2>>$ERRORFILE
 
 if check_install ansible-tower; then echo -e "ansible-tower already installed"; else install_AnsibleTower ansible-tower; fi
+
+if check_install azure-cli; then echo -e "azure-cli already installed"; else install_AzureCli azure-cli; fi
